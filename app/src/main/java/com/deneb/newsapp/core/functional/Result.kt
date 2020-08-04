e829361bed376240e1ae9eab6d7a9aa5f0cdf5e8
David Amador Moreno <amador.moreno.david@gmail.com>
2020-08-05 01:28:45 +0200

    fix versions

---
+++ app/src/main/java/com/deneb/newsapp/core/functional/Result.kt
@@ -0,0 +1,37 @@
+package com.deneb.newsapp.core.functional
+
+import androidx.lifecycle.MutableLiveData
+
+sealed class Result<out R> {
+
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val exception: Exception) : Result<Nothing>()
+    object Loading : Result<Nothing>()
+
+    override fun toString(): String {
+        return when (this) {
+            is Success<*> -> "Success[data=$data]"
+            is Error -> "Error[exception=$exception]"
+            Loading -> "Loading"
+        }
+    }
+}
+
+/**
+ * [Success.data] if [Result] is of type [Success]
+ */
+fun <T> Result<T>.successOr(fallback: T): T {
+    return (this as? Result.Success<T>)?.data ?: fallback
+}
+
+val <T> Result<T>.data: T?
+    get() = (this as? Result.Success)?.data
+
+/**
+ * Updates value of [liveData] if [Result] is of type [Success]
+ */
+inline fun <reified T> Result<T>.updateOnSuccess(liveData: MutableLiveData<T>) {
+    if (this is Result.Success) {
+        liveData.value = data
+    }
+}
