e017c47bc82fb822e066b4b7bad806f825c88ec5
David Amador Moreno <amador.moreno.david@gmail.com>
2020-08-09 11:54:21 +0200

    change to kotlin dsl

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+
+    val kotlin_version by extra("1.3.72")
+    repositories {
+        google()
+        jcenter()
+        
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        // Si necesitamos introducir con url
+        //maven { url = uri("https://jitpack.io") }
+    }
+}
+
+tasks.register("clean",Delete::class){
+    delete(rootProject.buildDir)
+}
